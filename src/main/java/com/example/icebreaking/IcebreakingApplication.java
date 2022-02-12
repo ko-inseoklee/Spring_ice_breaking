4dbedbf89639d954bf28f71e72b7e76d0818c072
Lee In Seok <tjrkd222@gmail.com>
2022-02-12 13:01:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/icebreaking/IcebreakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.icebreaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcebreakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcebreakingApplication.class, args);
+	}
+
+}
